597da9c6960cc6e1ef371663630d92f7daf2dada
joelcm20 <cjoel1977@gmail.com>
2024-01-08 17:35:26 -0300

    changes on character card
    show character name inside the image

--- challenge-dev/src/components/character/Character.jsx
+++ challenge-dev/src/components/character/Character.jsx
@@ -4,6 +4,7 @@ import ModalContext from "../../context/modal/modalContext"
 import Card from "react-bootstrap/Card";
 import { setSelectedCharacter } from "../../context/character/actions";
 import { setShowDetailModal } from "../../context/modal/actions"
+import style from "./Character.module.css"
 
 export default function Character({ character }) {
     const { dispatch: characterDispatch } = useContext(CharacterContext);
@@ -19,10 +20,10 @@ export default function Character({ character }) {
 
     return (
         <div className="col-12 col-sm-6 col-md-3">
-            <Card onClick={handleSetSelectedCharacter}>
-                <Card.Img variant="top" src={character.image} />
-                <Card.Body>
-                    <Card.Title className="text-center">{character.name}</Card.Title>
+            <Card className={`${style.card} mb-3`} onClick={handleSetSelectedCharacter}>
+                <Card.Img variant="" src={character.image} />
+                <Card.Body className={style.cardBody}>
+                    <Card.Title className="text-center text-white">{character.name}</Card.Title>
                 </Card.Body>
             </Card>
         </div>
